d337f56f6282ff8d4c0da0d40afb22d740958cb3
GitHub Action <action@github.com>
2023-10-06 08:11:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/ninoseki/intellijshadoenv/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ninoseki/intellijshadoenv/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ninoseki.intellijshadoenv.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
